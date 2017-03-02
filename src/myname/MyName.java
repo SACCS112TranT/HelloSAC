7fe18bc3b52e9dedb9bcead9f3c6b92e24650077
User <User@Nada>
2017-03-01 22:13:13 -0800

    ScannerNumbers

---
+++ src/myname/MyName.java
@@ -0,0 +1,24 @@
+/*/s
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myname;
+
+/**
+ *
+ * @author User
+ */
+public class MyName {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+      String greeting = "Hello World";
+      
+      System.out.printf("%s\n", greeting);  
+    }
+    
+}
